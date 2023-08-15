017c2160b7c64d02d7cc54172aea437c252087f1
tiktokyi <tiktokyi@gmail.com>
2023-08-15 21:20:11 +0900

    Feat : Users 클래스 생성

---
+++ src/main/java/com/example/footcare/Model/Users.java
@@ -0,0 +1,19 @@
+package com.example.footcare.Model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Table(name = "USERS")
+@Entity
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column
+    String name;
+
+    @Column
+    String password;
+}
