f21cbc80d90da17acdd87d83d19cbd61cea53a6c
tiktokyi <tiktokyi@gmail.com>
2023-08-15 21:43:12 +0900

    Feat : AuthController에 createUser 생성

--- src/main/java/com/example/footcare/Controller/AuthController.java
+++ src/main/java/com/example/footcare/Controller/AuthController.java
@@ -1,9 +1,16 @@
 package com.example.footcare.Controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 public class AuthController {
+
+    @PostMapping("/auth")
+    public ResponseEntity<String> createUser() {
+        return ResponseEntity.ok("생성 완료");
+    }
 }
